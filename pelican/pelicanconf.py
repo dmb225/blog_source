a044429360a3b8c183a9a118bf927d4bb35e2c1f
dmb225 <daomalick225@gmail.com>
2025-08-28 12:21:44 +0200

    Add social links

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -20,13 +20,12 @@ LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+    ("LinkedIn", "https://www.linkedin.com/in/dao-malick-benin/"),
+    ("GitHub", "https://github.com/dmb225"),
 )
 
 DEFAULT_PAGINATION = 10
